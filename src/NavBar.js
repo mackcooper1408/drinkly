1e8963251a2b110018be2c5bf861fe81deb71f59
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-11-27 13:59:58 -0800

    added navbar

---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <Navbar className="NavBar" bg="light">
+      <Navbar.Brand href="/">Drinks Up!</Navbar.Brand>
+      {/* <Nav className="ml-auto">
+        <Nav.Link>jj</Nav.Link>
+        <Nav.Link>hh</Nav.Link>
+      </Nav> */}
+    </Navbar>
+  );
+}
+
+export default NavBar;
