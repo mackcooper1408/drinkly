5c18294b2f11e0051c60973a9ad2fbc6092edaf6
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-23 19:40:24 -0800

    updated DrinkDetails component styling and functionality to move items from 'need' list to 'have' list

--- src/DrinkDetails.js
+++ src/DrinkDetails.js
@@ -1,22 +1,25 @@
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
-// import { Button, ListGroup } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 import DrinkApi from "./api";
 import "./DrinkDetails.css";
 
-
+/** Displays details about a specific cocktail.
+ *  Shows list of needed ingredients, list of owned ingredients,
+ *  a photo of the drink, and a recipe for making the drink */
 function DrinkDetails() {
   const [drink, setDrink] = useState({});
-  const [neededIngredients, setNeededIngredients] = useState([])
-  const [ownedIngredients, setOwnedIngredients] = useState([])
+  const [neededIngredients, setNeededIngredients] = useState([]);
+  const [ownedIngredients, setOwnedIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const { id } = useParams();
 
   useEffect(() => {
     async function getDrinkDetails() {
+      // get the drink details from the API based on ID in URL params
       const result = await DrinkApi.getDrink(id);
 
       const recipe = getRecipe(result);
@@ -26,8 +29,12 @@ function DrinkDetails() {
       setIsLoading(false);
     }
     getDrinkDetails();
-  }, []);
+  }, [id]);
 
+  /** extracts recipe from data returned by API
+   *  instead returns as an array ->
+   *  ["measurement ingredient", "other-measurement other-ingredient", ...]
+   *  @param {object} drink - drink data from API */
   function getRecipe(drink) {
     const recipe = [];
     let i = 1;
@@ -36,21 +43,24 @@ function DrinkDetails() {
         const measure = drink[`strMeasure${i}`];
         const ingredient = drink[`strIngredient${i}`];
         const instructions = `${measure} ${ingredient}`;
-        recipe.push(instructions);
+        recipe.push({ id: uuid(), instructions });
       }
       i++;
     }
     return recipe;
   }
 
+  /** removes ingredient from "needed" list to "owned" list */
   function handleClick(evt) {
-    const item = evt.target.parentNode;
-    console.log(item.innerText);
+    const itemId = evt.target.id;
+    const itemText = evt.target.innerText;
 
-    const neededMinusItem = neededIngredients.filter(inst => inst !== item.innerText);
+    const neededMinusItem = neededIngredients.filter(
+      (inst) => String(inst.id) !== itemId
+    );
 
     setNeededIngredients(neededMinusItem);
-    setOwnedIngredients(old => [...old, item.innerText]);
+    setOwnedIngredients((old) => [...old, itemText]);
   }
 
   if (isLoading) return <h1>Loading...</h1>;
@@ -58,38 +68,53 @@ function DrinkDetails() {
   return (
     <div className="DrinkDetails container">
       <h3>{drink.strDrink}</h3>
+      <img
+        src={drink.strDrinkThumb}
+        alt={drink.strDrink}
+        width="80%"
+        height="auto"
+      ></img>
       <div className="row row-sm">
-        <div className="col-5">
-          <h5>Ingredients Needed</h5>
+        <div className="col my-2">
+          <h3>Ingredients</h3>
+        </div>
+      </div>
+      <div className="row row-sm">
+        <div className="col-6">
+          <h5>Need</h5>
           <ul className="list-group">
-            {neededIngredients.map(inst => (
-              <li className="list-item" key={inst}>
-                <FontAwesomeIcon icon={faPlusCircle} className="float-left" onClick={handleClick} />
-                {inst}
-              </li>
-            ))}
-          </ul>
-          <h5>Ingredients checked</h5>
-          <ul className="list-group">
-            {ownedIngredients.map(inst => (
-              <li className="list-item" key={inst}>
-                <i className="float-left">
-                  ...
-                  </i>
-                {inst}
-              </li>
+            {neededIngredients.map((inst) => (
+              <div
+                className="btn btn-warning btn-sm my-1"
+                id={inst.id}
+                key={inst.id}
+                onClick={handleClick}
+              >
+                {/* <FontAwesomeIcon icon={faPlusCircle} className="float-left" /> */}
+                {inst.instructions}
+              </div>
             ))}
           </ul>
         </div>
         <div className="col-6">
-          <h5>Instructions</h5>
-          <img src={drink.strDrinkThumb} alt={drink.strDrink} width="25%" height="auto"></img>
-          <h5>{drink.strInstructions}</h5>
-          <button className="btn btn-primary btn-lg">button</button>
+          <h5>Have</h5>
+          <ul className="list-group">
+            {ownedIngredients.map((inst) => (
+              <div className="btn btn-success btn-sm my-1" key={inst}>
+                <i className="float-left">...</i>
+                {inst}
+              </div>
+            ))}
+          </ul>
         </div>
       </div>
+      <hr />
+      <div className="col mb-4">
+        <h3>Instructions</h3>
+        <h5>{drink.strInstructions}</h5>
+      </div>
     </div>
-  )
+  );
 }
 
 export default DrinkDetails;
