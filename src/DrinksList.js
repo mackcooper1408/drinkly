b762d745d060a80abe6ab67cc54fbef4cdf79aec
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-23 18:38:36 -0800

    removed all react-bootstrap components and simplified to show a simple list of items

--- src/DrinksList.js
+++ src/DrinksList.js
@@ -1,29 +1,20 @@
-import React, { useState } from "react";
-import { Button, ListGroup } from "react-bootstrap";
+import React from "react";
 import { Link } from "react-router-dom";
-import CocktailPicker from "./CocktailPicker";
 
-
-function DrinksList() {
-  const [drinks, setDrinks] = useState([]);
-
-
-  function updateList(drinkData) {
-    setDrinks(drinkData);
-  }
-
+/** Displays list of drinks from selected ingredients.
+ *  Each drink links to a drink detail page
+ */
+function DrinksList({ drinks }) {
   return (
     <div>
-      {drinks && drinks.length > 0 ? 
-      <ListGroup className="my-4" as="ul">
-        <Button className="my-2" onClick={() => setDrinks([])}>Reset</Button>
-        {drinks && drinks.map(d => (
-          <ListGroup.Item as="li" key={d.idDrink}>
-            <Link to={`/drinks/${d.idDrink}`}>{d.strDrink}</Link>
-          </ListGroup.Item>
-        ))}
-      </ListGroup> :
-      <CocktailPicker updateList={updateList} />}
+      <ul className="list-group my-4">
+        {drinks &&
+          drinks.map((d) => (
+            <li className="list-group-item" key={d.idDrink}>
+              <Link to={`/drinks/${d.idDrink}`}>{d.strDrink}</Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
