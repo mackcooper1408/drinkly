54dafb42d45f7007df41f84191ec250b302b107f
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-28 16:58:17 -0800

    added SearchBar component at top of render and search function for filtering ingredients

--- src/IngredientPicker.js
+++ src/IngredientPicker.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDrinksFromAPI } from "./actions/drinks";
 import { getIngredientsFromAPI } from "./actions/ingredients";
+import SearchBar from "./SearchBar";
 
 /** Displays a subset of ingredients to chose from */
 function IngredientPicker() {
-  // TODO: add ability to select more options
   const ingredients = useSelector((store) => store.ingredients);
-  const slicedIngredients = ingredients.slice(0, 10);
+  const [searched, setSearched] = useState(ingredients);
+  const slicedIngredients =
+    searched.length > 0 ? searched.slice(0, 10) : ingredients.slice(0, 10);
 
   const dispatch = useDispatch();
 
@@ -17,17 +19,31 @@ function IngredientPicker() {
 
   // updates drinks in redux store depending on ingredient selections
   function updateDrinks(evt) {
-    // TODO: fix to filter listinstead of change entire list
+    // TODO: fix to filter list instead of change entire list
     const val = evt.target.innerText;
     dispatch(getDrinksFromAPI(val));
+  }
+
+  function search(term) {
+    if (term.length > 0) {
+      const newIngredients = ingredients.filter((i) =>
+        i.strIngredient1.toLowerCase().includes(term.toLowerCase())
+      );
+      setSearched(newIngredients);
+    }
   }
 
   if (ingredients.length === 0) return <div>LOADING...</div>;
 
   return (
     <div>
+      <SearchBar search={search} />
       {slicedIngredients.map((ingredient) => (
-        <button className="btn btn-sm btn-success m-2" onClick={updateDrinks}>
+        <button
+          className="btn btn-sm btn-success m-2"
+          key={ingredient.strIngredient1}
+          onClick={updateDrinks}
+        >
           {ingredient.strIngredient1}
         </button>
       ))}
