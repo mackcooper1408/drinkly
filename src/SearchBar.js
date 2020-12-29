01feb14cc58b345f8e4616de80509b4658ecfc7c
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-28 16:53:12 -0800

    added search bar component

---
+++ src/SearchBar.js
@@ -0,0 +1,45 @@
+import React, { useState, useRef } from "react";
+import { throttle } from "throttle-debounce";
+
+
+function SearchBar({ search }) {
+  const initialState = { term: "" };
+  const [formData, setFormData] = useState(initialState);
+
+  const throttleSearch = useRef(throttle(1000, (formData) => {
+    search(formData.term)
+  }));
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    search(formData.term);
+    setFormData(initialState);
+  }
+
+
+  /** Update local state w/curr state of input elem */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    const newFormData = { ...formData, [name]: value };
+
+    throttleSearch.current(newFormData);
+    setFormData(newFormData);
+  }
+
+  return (
+    <form className="form-inline mt-4" onSubmit={handleSubmit}>
+      <div className="form-group mx-2">
+        <input
+          type="text"
+          className="form-control"
+          name="term"
+          value={formData.term}
+          placeholder="Search Term..."
+          onChange={handleChange} />
+      </div>
+      <button className="btn btn-primary btn-md form-group">🔍 Search</button>
+    </form>
+  )
+}
+
+export default SearchBar;
