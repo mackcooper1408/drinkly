ebaa17bdde9d946a6bd484e159468b50330ace58
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-28 16:57:24 -0800

    updated to get drinks from redux and dispatch to redux store with useEffect at start of app use when drinks list is empty. Will pull in popular drinks from API

--- src/DrinksList.js
+++ src/DrinksList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getDrinksFromAPI } from "./actions/drinks";
 
 /** Displays list of drinks from selected ingredients.
- *  Each drink links to a drink detail page
- */
-function DrinksList({ drinks }) {
+ *  Each drink links to a drink detail page */
+function DrinksList() {
+  const drinks = useSelector((store) => store.drinks);
+
+  const dispatch = useDispatch();
+
+  // update store with popular drinks from API if no search has been made yet
+  useEffect(() => {
+    if (drinks.length === 0) dispatch(getDrinksFromAPI());
+  }, [dispatch, drinks]);
+
+  // display message when api returns "None Found" from drink search
+  if (drinks === "None Found")
+    return (
+      <div className="my-2">No Drinks Found With These Ingredients...</div>
+    );
+
   return (
     <div>
       <ul className="list-group my-4">
