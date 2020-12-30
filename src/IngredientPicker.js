32ebca130bb71459e0865384bde77ea39d254811
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-29 20:15:02 -0800

    updated handleClick function to add selections to redux

--- src/IngredientPicker.js
+++ src/IngredientPicker.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getDrinksFromAPI } from "./actions/drinks";
-import { getIngredientsFromAPI } from "./actions/ingredients";
+import {
+  addIngredientToSelections,
+  getIngredientsFromAPI,
+} from "./actions/ingredients";
 import SearchBar from "./SearchBar";
 
-/** Displays a subset of ingredients to chose from */
+/**
+ * Displays a subset of ingredients to choose from
+ */
 function IngredientPicker() {
   const ingredients = useSelector((store) => store.ingredients);
-  const [searched, setSearched] = useState(ingredients);
+  const [searched, setSearched] = useState([]);
   const slicedIngredients =
     searched.length > 0 ? searched.slice(0, 10) : ingredients.slice(0, 10);
 
@@ -17,13 +21,21 @@ function IngredientPicker() {
     if (ingredients.length === 0) dispatch(getIngredientsFromAPI());
   }, [dispatch, ingredients]);
 
-  // updates drinks in redux store depending on ingredient selections
-  function updateDrinks(evt) {
-    // TODO: fix to filter list instead of change entire list
+  /**
+   * update selections list and update drinks list with
+   * drinks matching selected ingredients
+   */
+  function handleClick(evt) {
     const val = evt.target.innerText;
-    dispatch(getDrinksFromAPI(val));
+
+    // update selections in redux store
+    dispatch(addIngredientToSelections(val));
   }
 
+  /**
+   * filters the list of ingredients by user input in search bar
+   * @param {string} term search term from search bar component
+   */
   function search(term) {
     if (term.length > 0) {
       const newIngredients = ingredients.filter((i) =>
@@ -37,17 +49,20 @@ function IngredientPicker() {
 
   return (
     <div className="container col-sm-12 col-md-6">
-      <h3><i>Pick Your Ingredients</i></h3>
-      <SearchBar search={search} />
+      {/* <h3>
+        <i>Pick Your Ingredients</i>
+      </h3> */}
+      <SearchBar search={search} placeholder="Search Ingredients..." />
       {slicedIngredients.map((ingredient) => (
         <button
           className="btn btn-sm btn-success m-2"
           key={ingredient.strIngredient1}
-          onClick={updateDrinks}
+          onClick={handleClick}
         >
           {ingredient.strIngredient1}
         </button>
       ))}
+      <hr className="border-secondary" />
     </div>
   );
 }
