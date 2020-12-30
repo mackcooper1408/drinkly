6d286579dc40f250f54a57f49620f668b4890d6b
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-12-29 20:12:53 -0800

    added SelectedIngredients component

--- src/CocktailPicker.js
+++ src/CocktailPicker.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import DrinksList from "./DrinksList";
 import IngredientPicker from "./IngredientPicker";
+import SelectedIngredients from "./SelectedIngredients";
 
 /** displays list of ingredients (IngredientPicker)
  *  and resulting cocktails (DrinksList) once an ingredient has been selected
  */
 function CocktailPicker() {
-
   return (
     <div>
       <IngredientPicker />
+      <SelectedIngredients />
       <DrinksList />
     </div>
   );
