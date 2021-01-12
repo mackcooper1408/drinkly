8272008d25d70d19c59a77e40d679bf72b336479
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-01-11 23:53:03 -0800

    check for duplication in selections

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -24,6 +24,7 @@ export default function rootReducer(state = INITIAL_STATE, action) {
         drinks: action.payload,
       };
     case ADD_SELECTIONS:
+      if (state.selections.includes(action.payload)) return state;
       return {
         ...state,
         selections: [...state.selections, action.payload],
