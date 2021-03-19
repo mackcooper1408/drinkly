5eb4ce722918e6cb73ce8b034a7e31db4b22d542
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2021-03-19 16:11:50 -0600

    update home styling

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,15 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="Home">
-      <Button><Link to="/drinks">Make Your Cocktail!</Link></Button>
+    <div className="Home d-flex flex-column text-lg">
+      <span className="Home-welcome py-2">Welcome to Drinkly!</span>
+      <Link to="/drinks">
+        <Button variant="outline-light" size="lg">
+          Make Your Cocktail!
+        </Button>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Home;
